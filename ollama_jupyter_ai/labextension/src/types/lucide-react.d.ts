1af790e7749bc90d405b82d4febd936e3a18095b
bhumukulraj <bhumukulraj.ds@gmai.com>
2025-03-08 07:32:30 +0530

    updated gitignore

---
+++ ollama_jupyter_ai/labextension/src/types/lucide-react.d.ts
@@ -0,0 +1,11 @@
+declare module 'lucide-react/dist/esm/icons/message-circle' {
+  import { FC, SVGProps } from 'react';
+  const MessageCircle: FC<SVGProps<SVGSVGElement>>;
+  export default MessageCircle;
+}
+
+declare module 'lucide-react/dist/esm/icons/send' {
+  import { FC, SVGProps } from 'react';
+  const Send: FC<SVGProps<SVGSVGElement>>;
+  export default Send;
+} 
